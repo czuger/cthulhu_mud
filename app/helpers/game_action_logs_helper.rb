7fec0b464d5df1492b25a63f13be41da8960fd5f
Zuger Cédric <zuger.cedric@gmail.com>
2016-01-24 19:11:01 +0100

    Working on actions logs.

--- app/helpers/game_action_logs_helper.rb
+++ app/helpers/game_action_logs_helper.rb
@@ -1,16 +1,17 @@
 module GameActionLogsHelper
 
   def current_location_name(log )
-    location = log.action_location
-    if location
-      location.name_with_partitive_article + ' ' + location.parent_with_locative_prep
+    @action_location = log.action_location
+    if @action_location
+      @action_location.name_with_partitive_article + ' ' + @action_location.parent_with_locative_prep
     end
   end
 
   def result_location_name(log )
     location = log.result_location
     if location
-      location.name_with_partitive_article + ' ' + location.parent_with_locative_prep
+      dry_location = (location.parent_id == @action_location.parent_id) ? '' : ' ' + location.parent_with_locative_prep
+      location.name_with_partitive_article + dry_location
     end
   end
 
