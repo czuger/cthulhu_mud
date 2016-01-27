4cf18d7634dd52036edcef01781e8f1f310dd495
Zuger Cédric <zuger.cedric@gmail.com>
2016-01-27 12:42:27 +0100

    Css modifs.

--- app/controllers/investigator_actions_controller.rb
+++ app/controllers/investigator_actions_controller.rb
@@ -4,7 +4,9 @@ class InvestigatorActionsController < ApplicationController
     @current_game_board = GameBoard.find(params[:game_board_id] )
     @investigator = Investigator.find( params[:id] || params[:investigator_id] )
     @investigator.game_action.check_action
-    @log = @current_game_board.game_board_logs.limit( 5 )
+    @board_log = @current_game_board.game_board_logs.limit( 5 )
+    @in_the_news_places = @current_game_board.in_the_news_places
+    @game_action_log = @investigator.game_action_logs
   end
 
 end
