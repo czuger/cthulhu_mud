c2386102c6f911128ffd3901253ce4a5181c3940
Zuger Cédric <zuger.cedric@gmail.com>
2016-01-31 00:44:17 +0100

    Bugfix

--- config/deploy.rb
+++ config/deploy.rb
@@ -39,8 +39,17 @@ set :linked_files, fetch(:linked_files, []).push( 'config/secrets.yml', 'db/prod
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
   task :restart do
-    invoke 'unicorn:restart'
+    invoke 'unicorn:legacy_restart'
   end
+end
+
+task :custom_restart do
+  puts 'About to stop unicorn'
+  invoke 'unicorn:stop'
+  sleep( 1 ) # To give unicorn the time to really stop
+  puts 'About to start unicorn'
+  invoke 'unicorn:start'
+  puts 'End starting unicorn'
 end
 
 namespace :deploy do
