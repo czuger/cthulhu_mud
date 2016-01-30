9cfcd60c39e943c56b19a15254852eb38f2f0468
ced <zuger.cedric@gmail.com>
2016-01-30 09:14:19 +0100

    Push and pull to dev.

---
+++ pull_db_to_dev.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+scp hw:/var/www/cthulhu_mud/prod/current/db/production.sqlite3 db/development.sqlite3
--- push_db_to_prod.sh
+++ push_db_to_prod.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
+ssh hw cp /var/www/cthulhu_mud/prod/current/db/production.sqlite3 /var/www/cthulhu_mud/prod/current/db/production.sqlite3.`date +%Y-%m-%d.%H:%M:%S`
+
 scp db/development.sqlite3 hw:/var/www/cthulhu_mud/prod/current/db/production.sqlite3
