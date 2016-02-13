09bbdffca3ebfe4a18b7b62a417511c296e09124
Zuger Cédric <zuger.cedric@gmail.com>
2016-02-13 22:20:13 +0100

    Removed a ruby 2.3 instruction

--- app/helpers/in_the_news_helper.rb
+++ app/helpers/in_the_news_helper.rb
@@ -7,7 +7,8 @@ module InTheNewsHelper
   end
 
   def target_travel_class( in_the_news )
-    if @investigator&.destination_id == in_the_news.place_id
+    # if @investigator&.destination_id == in_the_news.place_id
+    if @investigator && @investigator_id == in_the_news.place_id
       :target_travel
     elsif @other_destinations_targeted_by_investigator.include?( in_the_news.place_id )
       :other_investigator_target_travel
